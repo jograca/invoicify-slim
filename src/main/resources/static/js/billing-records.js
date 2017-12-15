37f4794f3ae8087b81faeb5b72a26f3e7e243df9
Jon Graca <jon.graca@gmail.com>
2017-12-15 11:47:36 -0500

    Adding in Additional JSON and Controllers

---
+++ src/main/resources/static/js/billing-records.js
@@ -0,0 +1,16 @@
+$(function () {
+	
+	$('#create-flat-fee-billing-form').submit(function (e) {
+		e.preventDefault();
+	}) 
+	
+}); 
+
+
+$(function () {
+	
+	$('#create-rate-based-billing-form').submit(function (e) {
+		e.preventDefault();
+	})
+	
+}); 
